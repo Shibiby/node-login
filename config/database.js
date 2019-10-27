55ce9378afc4afe515ef89c396b0eb2a6001313f
Muhammad Feisal Shibiby <37222375+Shibiby@users.noreply.github.com>
2019-10-27 22:29:00 +0300

    commit

---
+++ config/database.js
@@ -0,0 +1,13 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': 'localhost', // localde iseniz bu , local değilse ip adresini yazınız .
+        'user': 'root', // kullanıcı adı 
+        'password': '123456', // şifreniz 
+        'database': 'node_login'// database ismi .
+    },
+	'database': 'node_login',
+    
+};
+
+	
